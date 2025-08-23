5176659fd3fda5b60b749b37675e7b0cf712a03d
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2025-08-23 13:36:07 -0700

    remove height

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import React from 'react'
+import { useState } from 'react'
+import cn from 'classnames'
 import { useMotionValueEvent, useScroll } from 'framer-motion'
 
 import { Wrapper } from '../Wrapper'
@@ -10,21 +11,24 @@ import { Languages } from './Languages'
 
 import styles from './Header.module.css'
 
-const Header: React.FC = () => {
-  const [scroll, setScroll] = React.useState(0)
+export const Header = () => {
+  const [scroll, setScroll] = useState(0)
+
   const { scrollY } = useScroll()
+
   useMotionValueEvent(scrollY, 'change', (latest) => {
     setScroll(latest)
   })
+
   return (
-    <header className={`${styles.container} ${scroll ? styles.scroll : ''}`}>
+    <header className={cn(styles.container, { [styles.scroll]: scroll })}>
       <Wrapper classes={styles.header}>
         <Contacts />
+
         <Logo scroll={scroll} />
+
         <Languages />
       </Wrapper>
     </header>
   )
 }
-
-export { Header }
