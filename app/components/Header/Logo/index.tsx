125c27cff6650dc6b8508821113d64283e5ccbdc
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2025-08-23 13:54:23 -0700

    font-size

--- app/components/Header/Logo/index.tsx
+++ app/components/Header/Logo/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { FC, useContext } from 'react'
+import cn from 'classnames'
 
 import { StoreContext } from '@/app/context'
 
@@ -7,17 +8,19 @@ import styles from './Logo.module.css'
 interface IProps {
   scroll: number
 }
-const Logo: React.FC<IProps> = ({ scroll }) => {
-  const store = React.useContext(StoreContext)
+
+export const Logo: FC<IProps> = ({ scroll }) => {
+  const store = useContext(StoreContext)
+
   if (!store) throw new Error('useStore must be used within a StoreProvider')
+
   const { state } = store
 
   return (
     <div className={styles.container}>
-      <h1 className={`${styles.container_h1} ${scroll ? styles.scroll : ''}`}>{state.logoFirst}</h1>
+      <h1 className={cn(styles.container_h1, { [styles.scroll]: scroll })}>{state.logoFirst}</h1>
+
       <h2 className={styles.container_h2}>- {state.logoSecond} -</h2>
     </div>
   )
 }
-
-export { Logo }
